367c6d332bfdf163b3362679e488bbbb9b170caa
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-10-16 22:36:43 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,34 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+function handleAddTask(newTask){
+  setTasks([...tasks, newTask]);
+}
+function handleDeleteTask(deletedText){
+  const updatedTasks = tasks.filter((task) => task.text !== deletedText);
+  setTasks(updatedTasks);
+}
+  const[selectedCategory, setSelectedCategory] = useState("All");
+
+  const filteredTasks = tasks.filter((task) => {
+    if (selectedCategory === "All") return true;
+    return task.category === selectedCategory;
+  });
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES}
+        selectedCategory={selectedCategory}
+        onSelectCategory={setSelectedCategory}
+      
+      />
+      <NewTaskForm categories={CATEGORIES} 
+        onTaskFormSubmit={handleAddTask}
+      />
+      <TaskList tasks={filteredTasks}
+        onDeleteTask={handleDeleteTask}
+      />
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React,{useState} from "react";
+import Task from "./Task";
 
-function TaskList() {
+
+function TaskList({tasks, onDeleteTask}) {
+
+
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
-    </div>
-  );
+      {tasks.map((task) => (
+        <Task 
+          key={task.text}
+          text={task.text}
+          category={task.category}
+          onDelete={() =>onDeleteTask(task.text)}
+        />
+      ))}
+      </div>
+    );
 }
 
-export default TaskList;
+  export default TaskList;
